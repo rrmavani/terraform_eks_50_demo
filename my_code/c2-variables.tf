903a912c18dffb527c1733090e4354a1cd0b0351
Rushabh <rrmavani@gmail.com>
2023-01-27 19:56:06 +0530

    updated

--- my_code/c2-variables.tf
+++ my_code/c2-variables.tf
@@ -1,5 +1,11 @@
 # Input Variables
 # AWS Region
+variable "my_environment" {
+  description = "Environment"
+  type = string
+  default = "dev"
+}
+
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
   type = string
@@ -18,4 +24,22 @@ variable "instance_keypair" {
   description = "AWS EC2 Key Pair that need to be associated with EC2 Instance"
   type = string
   default = "terraform-key"
+}
+
+# AWS EC2 Instance Type - List
+variable "instance_type_list" {
+  description = "EC2 Instnace Type"
+  type = list(string)
+  default = ["t2.micro", "t1.micro"]
+}
+
+# AWS EC2 Instance Type - Map
+variable "instance_type_map" {
+  description = "EC2 Instnace Type"
+  type = map(string)
+  default = {
+    "dev" = "t2.micro"
+    "qa"  = "t1.micro"
+    "prod" = "t3.micro"
+  }
 }
--- my_code/c6-outputs.tf
+++ my_code/c6-outputs.tf
@@ -3,11 +3,16 @@
 # EC2 Instance Public IP
 output "instance_publicip" {
   description = "EC2 Instance Public IP"
-  value = aws_instance.myec2vm.public_ip
+  value = aws_instance.myec2vm[*].public_ip
 }
 
 # EC2 Instance Public DNS
 output "instance_publicdns" {
   description = "EC2 Instance Public DNS"
-  value = aws_instance.myec2vm.public_dns
+  value = ["${aws_instance.myec2vm[*].tags_all.Name}", "${aws_instance.myec2vm[*].public_ip}", "${aws_instance.myec2vm.*.public_dns}"]
+}
+
+output "Instance_info" {
+  description = "information"
+  value = {for instance in aws_instance.myec2vm: instance.id => instance.public_dns}
 }
